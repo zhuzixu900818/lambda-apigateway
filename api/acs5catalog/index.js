724f02abe62f9429f189f74b3c858c389003a738
zhuzixu900818 <zhunikki818@gmail.com>
2021-01-21 14:55:16 -0700

    Update index.js

--- api/acs5catalog/index.js
+++ api/acs5catalog/index.js
@@ -2,9 +2,9 @@ const pg = require('pg');
 
 var connectionString = {
   user: 'cfapi',
-  host: 'sp63kb5w0r8p3p.ckswd2zr0ndt.us-west-2.rds.amazonaws.com',
-  database: 'shift_public',
-  password: 'cfapi',
+  host: 'host',
+  database: 'db',
+  password: 'pw',
   port: 5432,
 };
 
